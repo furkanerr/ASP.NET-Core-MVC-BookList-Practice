09ba657c3293c13f2009020ed46aadc99f214fc6
furkaner <furkaner.cbu@gmail.com>
2020-08-20 16:44:59 +0300

    ASP.NET Core Practice  BookList

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class AppDbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<Book> Books { get; set; }
+    }
+}
